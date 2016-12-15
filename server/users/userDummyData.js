b6e9444127092dd586c776c66631b562a4f6a5ea
Vernon Quan <vernonquan@gmail.com>
2016-12-15 12:01:58 -0800

    Added dummy data with different ranks

---
+++ server/users/userDummyData.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    email: 'vernon@hackreactor.com',
+    username: 'Vernon',
+    password: 'vernon',
+    rank: 5,
+    origin: 'San Francisco',
+    favoriteActivity: 'Vidja Gemes'
+  },
+  {
+    email: 'blake@hackreactor.com',
+    username: 'Blake',
+    password: 'blake',
+    rank: 7,
+    origin: 'H-Town',
+    favoriteActivity: 'Blaze it!'
+  },
+  {
+    email: 'rob@hackreactor.com',
+    username: 'Robert',
+    password: 'rob',
+    rank: 6,
+    origin: 'Not San Francisco',
+    favoriteActivity: 'Drank'
+  },
+  {
+    email: 'fiona@hackreactor.com',
+    username: 'Fiona',
+    password: 'fiona',
+    rank: 10,
+    origin: 'San Francisco',
+    favoriteActivity: 'EVERYTHING'
+  }
+];
