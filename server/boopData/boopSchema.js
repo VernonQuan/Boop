803dc7f2f7247eb0c6e35c39b8d1da7742379431
Vernon Quan <vernonquan@gmail.com>
2016-12-14 19:00:41 -0800

    Minor changes to boopSchema

--- server/boopData/boopSchema.js
+++ server/boopData/boopSchema.js
@@ -13,9 +13,9 @@ var boopSchema = new Schema({
   weekdays: [Boolean], //0 is Sunday, 6 is Saturday
   startDate: Date,
   limit: Number,
-  joinedUsers: Array,
+  joinedUsers: [String],
   endDate: [Date],
-  userId:  {type: Schema.Types.ObjectId, ref: 'User'},
+  ownerId:  {type: Schema.Types.ObjectId, ref: 'User'},
   checkIns: [Date] // a history of successful check-ins
   //time (when during the day are you supposed to check in)
   //repeats (every week? every other week? is this necessary?)
